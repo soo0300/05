bc28e9a582a9b028acee923ef59ec770fee5e577
soo0300 <soo0300@naver.com>
2022-10-04 16:36:02 +0900

    실습2

--- main.c
+++ main.c
@@ -6,13 +6,10 @@ int main(int argc, char *argv[]) {
 	printf("정수 하나를 입력하시오: ");
 	scanf("%d",n);
 	
-	if(n>0){
-		printf("양수입니다");
-	} 
-	else if(n==0){
-		printf("0 입니다");
+	if(n<0){
+		printf("절대값은 %d입니다 ",-n);
 	}else{
-		printf("음수입니다");
+		printf("절대값은 %d 입니다",n);
 	}
 	return 0;
  
