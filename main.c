b87af4c1f7a8a0916340cc4223aaa21d09e90e70
soo0300 <soo0300@naver.com>
2022-10-04 17:11:26 +0900

    실습4

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-	int n;
-	printf("정수 하나를 입력하시오: ");
-	scanf("%d",n);
+int main(int argc, char*argv[]){
+	int n; int i;
+
+	int sum=0;
+	printf("input a number: ");
+	scanf("%d",&n);
 	
-	if(n<0){
-		printf("절대값은 %d입니다 ",-n);
-	}else{
-		printf("절대값은 %d 입니다",n);
+	for(i=1; i<=n; i++){
+		sum+=i;
 	}
+	printf("The result is %d",sum);
+
 	return 0;
- 
-}
+
+
+} 
