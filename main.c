6755c84c4a46274278883df1a835131bac1576b4
soo0300 <soo0300@naver.com>
2022-10-04 17:14:00 +0900

    실습1_최종

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char*argv[]){
-	int n; int i;
-
-	int sum=0;
-	printf("input a number: ");
+int main(int argc, char *argv[]) {
+	int n;
+	printf("정수 하나를 입력하시오: ");
 	scanf("%d",&n);
-	
-	for(i=1; i<=n; i++){
-		sum+=i;
-	}
-	printf("The result is %d",sum);
 
+	if(n>0){
+		printf("양수입니다");
+	} 
+	else if(n==0){
+		printf("0 입니다");
+	}else{
+		printf("음수입니다");
+	}
 	return 0;
 
-
-} 
+}
